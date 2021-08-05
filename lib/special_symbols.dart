10b847854bf7bc73005987baa36c6f60683c2656
ankushmishra2903-official <ankushmishra2903.official@gmail.com>
2021-08-05 12:56:02 +0530

    initial commit

---
+++ lib/special_symbols.dart
@@ -0,0 +1,107 @@
+library special_symbols;
+class SpecialSymbols {
+  /// Special Symbols of indianRupee ₹
+  static final String indianRupee = '₹';
+
+  /// Special Symbols of bulletPoint •
+  static final String bulletPoint = '•';
+
+  /// Special Symbols of bulletPointTriangle ‣
+  static final String bulletPointTriangle = '‣';
+
+  /// Special Symbols of bulletPointEmpty ○
+  static final String bulletPointEmpty = '○';
+
+  /// Special Symbols of doubleBulletPointEmpty ⦾
+  static final String doubleBulletPointEmpty = '⦾';
+
+  /// Special Symbols of doubleBulletPoint ⦿
+  static final String doubleBulletPoint = '⦿';
+
+  /// Special Symbols of copyright ©
+  static final String copyright = '©';
+
+  /// Special Symbols of registered ®
+  static final String registered = '®';
+
+  /// Special Symbols of om ॐ
+  static final String om = 'ॐ';
+
+  /// Special Symbols of degreeCelsius ℃
+  static final String degreeCelsius = '℃';
+
+  /// Special Symbols of degreeFahrenheit ℉
+  static final String degreeFahrenheit = '℉';
+
+  /// Special Symbols of ohm Ω
+  static final String ohm = 'Ω';
+
+  /// Special Symbols of ohmInverted ℧
+  static final String ohmInverted = '℧';
+
+  /// Special Symbols of arrowLeft ←
+  static final String arrowLeft = '←';
+
+  /// Special Symbols of arrowRight →
+  static final String arrowRight = '→';
+
+  /// Special Symbols of arrowUp ↑
+  static final String arrowUp = '↑';
+
+  /// Special Symbols of arrowDown ↓
+  static final String arrowDown = '↓';
+
+  /// Special Symbols of arrowLeftRight ↔
+  static final String arrowLeftRight = '↔';
+
+  /// Special Symbols of arrowUpDown ↕
+  static final String arrowUpDown = '↕';
+
+  /// Special Symbols of doubleArrowLeft ⇐
+  static final String doubleArrowLeft = '⇐';
+
+  /// Special Symbols of doubleArrowRight ⇒
+  static final String doubleArrowRight = '⇒';
+
+  /// Special Symbols of doubleArrowUp ⇑
+  static final String doubleArrowUp = '⇑';
+
+  /// Special Symbols of doubleArrowDown ⇓
+  static final String doubleArrowDown = '⇓';
+
+  /// Special Symbols of doubleArrowLeftRight ⇔
+  static final String doubleArrowLeftRight = '⇔';
+
+  /// Special Symbols of doubleArrowUpDown ⇕
+  static final String doubleArrowUpDown = '⇕';
+
+  /// Special Symbols of forAll ∀
+  static final String forAll = '∀';
+
+  /// Special Symbols of partialDifferential ∂
+  static final String partialDifferential = '∂';
+
+  /// Special Symbols of thereExists ∃
+  static final String thereExists = '∃';
+
+  /// Special Symbols of thereNotExists ∄
+  static final String thereNotExists = '∄';
+
+  /// Special Symbols of squareRoot √
+  static final String squareRoot = '√';
+
+  /// Special Symbols of cubeRoot ∛
+  static final String cubeRoot = '∛';
+
+  /// Special Symbols of infinity ∞
+  static final String infinity = '∞';
+
+  /// Special Symbols of notEqual ≠
+  static final String notEqual = '≠';
+
+  /// Special Symbols of lessThenOrEqualTo ≤
+  static final String lessThenOrEqualTo = '≤';
+
+  /// Special Symbols of greaterThenOrEqualTo ≥
+  static final String greaterThenOrEqualTo = '≥';
+}
